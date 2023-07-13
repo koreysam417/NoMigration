097e05e75014cfbee7b028c9988a5bb3227d79d5
Korey Sam <Korey.Sam@novanta.com>
2023-07-13 12:06:53 -0400

    Add project files.

---
+++ Data/BloggieDbContext.cs
@@ -0,0 +1,18 @@
+﻿using Microsoft.EntityFrameworkCore;
+using LicenseMgr.Models.Domain;
+
+namespace LicenseMgr.Data
+
+{
+    public class BloggieDbContext : DbContext
+    {
+        public BloggieDbContext(DbContextOptions options) : base(options)
+        {
+        }
+
+
+        public DbSet<BlogPost> BlogPosts { get; set; }
+        public DbSet<Tag> Tags { get; set; }
+    }
+}
+
